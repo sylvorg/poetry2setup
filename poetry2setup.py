7451b296a7a855e8400d7f250bce4da6dba42399
Jeet Ray <titaniumfiles@outlook.com>
2023-01-14 08:41:59 +0000

    Path now expands environment variables and users, and checks if the path actually exists.

--- poetry2setup.py
+++ poetry2setup.py
@@ -5,14 +5,22 @@ from pathlib import Path
 from poetry.core.factory import Factory
 from poetry.core.masonry.builders.sdist import SdistBuilder
 
+from os.path import expandvars
+
+
+def erPath(path):
+    if path:
+        return (path if isinstance(path, Path) else Path(expandvars(path))).expanduser().resolve(strict = True)
+    else:
+        return path
 
 
 def build_setup_py(path = "."):
-    return SdistBuilder(Factory().create_poetry(Path(path).resolve())).build_setup()
+    return SdistBuilder(Factory().create_poetry(erPath(path))).build_setup().decode("utf8")
 
 
 def main():
-    print(build_setup_py().decode("utf8"))
+    print(build_setup_py())
 
 
 if __name__ == "__main__":
